5bc474466d7144cca39db539d2dc986fa7e24fdb
unknown <joeyzhouqihui@gmail.com>
2020-07-16 15:20:57 +0800

    1

--- preprocess.py
+++ preprocess.py
@@ -9,8 +9,16 @@ from clause_generator import *
 3. 生成一个实验命令，这个命令一开始会 load 上面保存的图数据，然后运行一句continuously match语句，然后逐条 create 剩下的一半边
 '''
 
-#要导入的图数据
-dir = '/data/share/users/legend/project/query-generator/WukongSLarge/base_edges.txt'
+'''
+要导入的图数据
+node : node id + node label
+edge : src node id + edge label + dst node id
+'''
+dir = '/data/share/users/legend/project/query-generator/WukongSLarge/'
+base_edges = 'base_edges.txt'
+nodes = 'node.txt'
+stream_edges = 'stream_edges.txt'
+query = 'query.txt'
 
 #生成要load的图，需要的一些预处理的命令
 preprocess_commands = 'command/preprocess.txt'
@@ -19,25 +27,14 @@ preprocess_commands = 'command/preprocess.txt'
 insert_commands = 'command/insert.txt'
 
 #前半个图的存放地点
-data_to_load = 'pre_data'
+data_to_load = 'base_graph'
 
 #graphflow-shell 路径
 shell_path = '~/graphdb/graphflow/build/install/graphflow/bin/graphflow-cli'
 
 #数据文件的分割符
-separator = '|'
+separator = ' '
 
-#用于做id的重新分配
-min_id_permit = 0
-
-#识别文件中的数据是边还是点
-filename_to_filetype = {}
-
-#对于每个类型的节点，因为id重分配了，所以要记一下id的起始偏移
-vertex_to_idoffset = {}
-
-vertex_files = []
-edge_files = []
 clause_gen = Clause_generator()
 
 '''
@@ -48,66 +45,22 @@ save_clause = 'save to dir \"{0}\";'
 load_clase = 'load from dir \"{0}\";'
 
 '''
-get the vertex and edge types from the filename first
-'''
-def parse_types():
-    for filename in os.listdir(dir):
-        tmp = filename.split('_')
-        if len(tmp) > 4 :
-            edge_files.append(filename)
-            filename_to_filetype[filename] = tmp[0:3]
-        else :
-            vertex_files.append(filename)
-            filename_to_filetype[filename] = tmp[0]
-
-'''
 convert all the vertex files to a single cypher commands file
 '''
-def generate_create_vertex_commands(save_file):
-    global min_id_permit
-    for file in vertex_files:
-        vertex_type = filename_to_filetype[file]
-        if not vertex_type in vertex_to_idoffset.keys():
-            vertex_to_idoffset[vertex_type] = min_id_permit
-
-        max_id_assigned = -1
-        with open(dir+file, 'r', encoding='utf-8') as f:
-            line = f.readline().strip('\n')
-            keys = line.split(separator)
-            line = f.readline().strip('\n')
-            while line:
-                values = line.split(separator)
-                id = int(values[0]) + min_id_permit
-                props = {}
-                for i, str_value in enumerate(values[1:]):
-                    if re.search('^[0-9]+$', str_value): value = int(str_value)
-                    elif re.search('^[0-9]*\.[0-9]*$', str_value): value = float(str_value)
-                    else: value = str_value
-                    props[keys[i]] = value
-                if len(props) == 0: props = None
-                clause = clause_gen.create_vertex(id, vertex_type, props)
-                save_file.write(clause+'\n')
-                if max_id_assigned < id: max_id_assigned = id
-                line = f.readline().strip('\n')
-
-        f.close()
-        min_id_permit = max_id_assigned + 1
-
-'''
-convert all the edge files to a single cypher commands file
-the edge here is simple no props considered
-'''
-def get_file_lines(filename):
+def generate_create_vertex_commands(node_file, save_file):
     count = 0
-    with open(filename, 'r', encoding='utf-8') as f:
-        line = f.readline()
+    with open(node_file, 'r', encoding='utf-8') as f:
+        line = f.readline().strip('\n')
         while line:
+            id, label = list(map(int, line.split(separator)))
+            clause = clause_gen.create_vertex(id, label)
+            save_file.write(clause + '\n')
             count += 1
-            line = f.readline()
     f.close()
-    return count
+    print('total lines :', count)
 
-def generate_create_edge_commands(preprocessing_file, insert_file):
+def generate_create_edge_commands(edge_file, save_file):
+    '''
     for file in edge_files:
         save_file = preprocessing_file
         from_type, edge_type, to_type = filename_to_filetype[file]
@@ -129,6 +82,7 @@ def generate_create_edge_commands(preprocessing_file, insert_file):
                 if count > 5:
                     save_file = insert_file
         f.close()
+    '''
 
 '''
 execute the command file generated using " echo command | shell "
@@ -154,7 +108,6 @@ def exec(command_file, batch = 1000):# 1000句一起执行
 
 if __name__ == '__main__' :
     '''
-    parse_types()
     preprocess_file = open(preprocess_commands, 'w', encoding='utf-8')
     insert_file = open(insert_commands, 'w', encoding='utf-8')
     #先创建节点
@@ -172,11 +125,11 @@ if __name__ == '__main__' :
     exec(preprocess_commands)
     '''
 
-    with open(dir, 'r', encoding='utf-8') as file:
-        line = file.readline()
-        print(line)
-        file.close()
-    
+    preprocess_file = open(preprocess_commands, 'w', encoding='utf-8')
+    generate_create_vertex_commands(dir+nodes, preprocess_file)
+    preprocess_file.close()
+
+
 
 
 
