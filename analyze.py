ed632702a0476c98cf74f9fbf7f80c03d7636f26
unknown <joeyzhouqihui@gmail.com>
2020-08-07 13:06:54 +0800

    1

--- analyze.py
+++ analyze.py
@@ -2,13 +2,13 @@ import numpy as np
 
 triggers = [10, 100, 1000]
 bzs = [10, 100, 1000]
-
+dir = "command_old/"
 for trigger in triggers:
     for bz in bzs:
         filename = str(trigger) + "_" + str(bz) + ".txt"
         sum = 0
         count = 0
-        with open(filename, 'r', encoding='utf-8') as f:
+        with open(dir + filename, 'r', encoding='utf-8') as f:
             line = f.readline()
             while line:
                 sum += float(line.strip())
