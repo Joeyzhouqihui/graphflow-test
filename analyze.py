88c9fd81782807f787ff1568c71e43303ea93d88
unknown <joeyzhouqihui@gmail.com>
2020-08-07 13:05:28 +0800

    1

---
+++ analyze.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+triggers = [10, 100, 1000]
+bzs = [10, 100, 1000]
+
+for trigger in triggers:
+    for bz in bzs:
+        filename = str(trigger) + "_" + str(bz) + ".txt"
+        sum = 0
+        count = 0
+        with open(filename, 'r', encoding='utf-8') as f:
+            line = f.readline()
+            while line:
+                sum += float(line.strip())
+                count += 1
+                line = f.readline()
+        print(filename, " : ", sum/count)
+
+
