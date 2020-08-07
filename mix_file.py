7eaf127b606c1b4dc8070c6ea7d5299a232979f0
unknown <joeyzhouqihui@gmail.com>
2020-08-07 14:07:36 +0800

    1

---
+++ mix_file.py
@@ -0,0 +1,30 @@
+file1 = open("stream_command_bz_10.txt", "r", encoding="utf-8")
+file2 = open("stream_command_bz_100.txt", "r", encoding="utf-8")
+file3 = open("stream_command_bz_1000.txt", "r", encoding="utf-8")
+
+file4 = open("stream_command_bz_mixed.txt", "w", encoding="utf-8")
+
+num1 = 200
+skip2 = 20
+num2 = 20
+skip3 = 4
+num3 = 5
+
+for i in range(num1):
+    line = file1.readline()
+    file4.write(line)
+for i in range(skip2):
+    line = file2.readline()
+for i in range(num2):
+    line = file2.readline()
+    file4.write(line)
+for i in range(skip3):
+    line = file3.readline()
+for i in range(num3):
+    line = file3.readline()
+    file4.readline()
+
+file1.close()
+file2.close()
+file3.close()
+file4.close()
